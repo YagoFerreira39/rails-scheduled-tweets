10e6226e2830a92ea3d7dd3f2daeef805f36fca5
Yago Ferreira <yferreira@hsolmkt.com.br>
2022-05-10 01:59:45 -0300

    Set Current User && Update Password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
   def index
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    else
-      redirect_to sign_up_path
-    end
   end
 end
